beff7d6ca5745a3116ed92a070be9fda306e954d
lucas <lucas@yun.io>
2013-09-09 19:00:38 +0800

    Add handle macro

--- include/erl_sentry.hrl
+++ include/erl_sentry.hrl
@@ -2,3 +2,15 @@
 
 -define(lift(Expr), erl_sentry_util:lift(fun() -> Expr end)).
 -define(unlift(Expr), erl_sentry_util:unlift(fun() -> Expr end)).
+
+-define(handle(Res),
+    case Res of
+        {ok, {{200,_},_,_}} ->
+                Res;
+        {ok, {Code,_,Body}} ->
+				error_logger:format("erl sentry errors code ~p body ~p", [Code, Body]),
+                {error,{Code,Body}};
+        {error, Reason} ->
+				error_logger:format("erl sentry errors ~p", [Reason]),
+                {error,Reason}
+    end).
